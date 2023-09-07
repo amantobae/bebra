983a9dc58b6d7700d5e2f1a177ede1993b2957f3
Aman <gorl431@gmail.com>
2023-09-07 20:28:44 +0600

    complete homework0

---
+++ main.dart
@@ -0,0 +1,147 @@
+
+void main (){
+  ////1
+// int a = 5;
+// double b =5.126;
+// print(a+b);
+// print(a-b);
+// print(a*b);
+// print(a/b);
+
+////2
+// int a = 5;
+// num b = 2;
+// print(a+b);
+// print(a-b);
+// a = 132;
+// b = 123;
+// print(a*b);
+// a = 78;
+// b = 2;
+// print(78/2);
+// b = 2.5;
+// print(a/b);
+// a = 0;
+// b = 6;
+// print(a*b);
+
+////3
+// int a = 5;
+// int b = 6;
+// a = a + b;
+// b = a - b;
+// a = b - a;
+// print('a = $a ; b = $b');
+
+////4
+// String a = "введите ваше имя";
+// String b = "введите ваш возраст";
+// print("$a \n$b");
+
+
+////7
+  // int a = 10 ;
+  // if (a > 3){
+  //   print(a+10);
+  // }
+  // else{
+  //   print(a-10);
+  // }
+
+///8
+  // int a = 9;
+  // if (a < 7){
+  //   print("yes");
+  // }
+  // else if (a > 10){
+  //   print("no");
+  // }
+  // else if (a == 9){
+  //   print("error");
+  // }
+ 
+////9
+  //  int a = 2;
+  //  int b = 3;
+  //  if (a > b){
+  //   print(a);
+  //  }
+  //  else if (b > a){
+  //   print(b);
+  //  }
+
+////10
+  // int a = 50;
+  // int b = 50;
+  // if (a + b == 100){
+  //   print("yes");
+  // }
+  // else {
+  //   print("no");
+  // }
+
+  // print('Введите номер месяца');
+
+  // String? a = stdin.readLineSync();
+  // int? b = int.tryParse(a!);
+  // if (b == 12 || b == 1 || b ==2 ){
+  // print('зима');
+  // }
+  // else if (b == 3 || b == 4|| b == 5 ){
+  //   print('весна');
+  // }
+  // else if (b == 6 || b == 7|| b == 8){
+  //   print("лето");
+  // }
+  // else if (b == 9 || b == 10 || b == 11){
+  //   print("осень");
+  // }
+
+  ////12
+  // int a = 21;
+  // int b = 10;
+  // int c = 14;
+  // if (a < b && a < c){
+  //   print(a);
+  // }
+  // else if (b < a && b < c){
+  //   print(b);
+  // }
+  // else if (c < a && c < b){
+  //   print(c);
+  // }
+
+////13
+  // int a = 123;
+  // String b = a.toString();
+  // List c = b.split('');
+  // c.insert(0, c.last);
+  // c.insert(3, c[1]);
+  // c.removeAt(1);
+  // c.removeAt(c.length-1);
+  // a = int.parse( c.join());
+
+  // print(a);
+
+  
+
+////5
+  // bool weather = false;
+  // weather == true ? print("бери зонт"): print("не бери зонт");
+ 
+////14
+  // List a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+  // for (int i = 0 ; i < a.length; i++)
+  // if (a[index] < 5){
+  //   print(a[index]);
+  // }
+    // String number = "123";
+  // if(number.length == 12){
+  //   print("верно");
+  // }
+  // else {
+  //   print("неверно");
+  // }
+
+
+}
---
+++ main1.dart
@@ -0,0 +1,13 @@
+void main (){
+  int a = 1;
+  switch (a) {
+    case 1:
+    print("!");
+    break;
+
+    case 2:
+    print("2");
+    break;
+  }
+}
+ 
